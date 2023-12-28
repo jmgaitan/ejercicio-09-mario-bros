974386b087114769de4499230fc2acb2d7b5610c
juan Manuel Gaitan <jmgaitan93@gmail.com>
2023-12-28 16:33:42 -0300

    tp 9 finalizado

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-
 function fValidateInput(x) {
     var nombreValido = true;
 
     if (x === 'Mario'|| x === 'Toad' || x === 'Toadette' || x === 'Daisy') {
-    } else if (x === 'Luigi') {
-    } else if (x === 'Bowser') {
+    } else if (x == 'Luigi') {
+    } else if (x == 'Bowser') {
         x = 'Bowser Morton Koopa';
-    } else if (x === 'Peach') {
+    } else if (x == 'Peach') {
         x = 'Princesa Peach Toadstool';
-    } else if (x === 'Yoshi') {
+    } else if (x == 'Yoshi') {
         x = 'T. Yoshisaur Munchakoopas';
     } else {
         nombreValido = false;
@@ -23,42 +22,44 @@ function searchActor(search) {
     divID.title = "Presentado";
 }
 
-
-function userInput (){
-    let cont
-    let actor
-    let name
-    let validateInput
-    
-    
-    do {
-        actor = prompt("¿Quién se presenta hoy?");
-        actor = actor.toLowerCase()
-    
-        name = actor.charAt(0).toUpperCase() + actor.slice(1);
-        validateInput = fValidateInput(name);
-        const miSpan = document.getElementsByTagName('span');
-        for (var i = 0; i < miSpan.length; i++ ){
-            miSpan[i].textContent = validateInput;
-        }
-        cont =+ 1 ;    
-    } while (validateInput ===null);
-    searchActor(actor);
-
-}
-
-function selectionActor() {
+function deleteActor() {
     const miDiv = document.getElementsByTagName('div');
     for (var i = 0; i < miDiv.length; i++ ){
         miDiv[i].removeAttribute("title");
     }
-userInput()
 }
 
 
-    
-    
+function actorPrompt(){
+    let name
+    let validateInput
+    let actor
+    do{
+        actor = prompt("¿Quién se presenta hoy?");
+        actor = actor.toLowerCase()
+        name = actor.charAt(0).toUpperCase() + actor.slice(1);
+        validateInput = fValidateInput(name);
+    }while (validateInput ===null);
+    insertSpan(validateInput)
+    searchActor(actor);
+}
 
-    
-    
+function insertSpan (presentador){
 
+        deleteActor()
+        const miSpan = document.getElementsByTagName('span');
+        for (var i = 0; i < miSpan.length; i++ ){
+            miSpan[i].textContent = presentador;
+        }       
+}
+
+    cajas.addEventListener("click", function (event) {
+            let idContent = event.target.id;
+            if (idContent && idContent !== 'cajas'){
+            const name = idContent.charAt(0).toUpperCase()+idContent.slice(1)
+            let ValidateId = fValidateInput(name);
+            insertSpan(ValidateId);
+            searchActor(idContent);
+        }
+            
+        })
